485db5655f23f1bb90e82c2abd87928d6af94509
klikki-lab <klikki.lab@gmail.com>
2024-04-02 17:46:18 +0900

    sakuraNoteの消滅時間の修正

--- src/game_scene/sakura/sakuraNote.ts
+++ src/game_scene/sakura/sakuraNote.ts
@@ -41,7 +41,7 @@ export class SakuraNote extends g.Sprite {
     };
 
     private destroyHandler = (): void => {
-        this.opacity *= .95;
+        this.opacity *= .8;
         this.modified();
         if (this.opacity < 0.01) {
             this.destroy();
